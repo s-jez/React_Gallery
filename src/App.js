77cf31cec22c4e88e61a939af867a4e1ce921a54
stanislawjez <stanislaw.jez@enterosoft.com>
2022-03-31 19:28:48 +0200

    [main] - change URLs pictures

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import Navbar from "./Navbar";
 
 function App() {
   const imagesMinecraft = [
-    "S_Nagrodzeni-za-Minecraft.6.jpg",
-    "S_Nagrodzeni-za-Minecraft.5.jpg",
-    "S_Nagrodzeni-za-Minecraft.4.jpg",
-    "S_Nagrodzeni-za-Minecraft.3.jpg",
-    "S_Nagrodzeni-za-Minecraft.2.jpg",
-    "S_Nagrodzeni-za-Minecraft.1.jpg",
-    "S_Nagrodzeni-za-Minecraft.9.jpg",
-    "S_Nagrodzeni-za-Minecraft.7.jpg",
-    "S_Nagrodzeni-za-Minecraft.8.jpg",
+    "b5/91/622b88b69a748_o_large.jpg",
+    "f5/c1/622b886a1c253_o_large.jpg",
+    "2b/73/622b888172294_o_large.jpg",
+    "06/4d/622b8883ed792_o_large.jpg",
+    "ff/c9/622b889381748_o_large.jpg",
+    "33/0b/622b889bec871_o_large.jpg",
+    "8d/11/622b889ea038d_o_large.jpg",
+    "80/82/622b88a1a5c05_o_large.jpg",
+    "cb/d0/622b88a79cb21_o_large.jpg",
+    "fe/60/622b88b09b35a_o_large.jpg",
+    "59/77/622b88b38cd19_o_large.jpg",
   ];
   const imagesTrip = [
     "20191127_123309-768x363.jpg",
@@ -22,7 +24,7 @@ function App() {
   ];
   const imageTripSecond = ["received_657971532075575-225x300.jpeg"];
   const buildMinecraftURL = (imagePath) =>
-    `https://info.kanal6.pl/wp-content/uploads/2022/03/${imagePath}`;
+    `https://d-art.ppstatic.pl/kadry/k/r/1/${imagePath}`;
   const buildTripURL = (imagePath) =>
     `https://new1.zsp1slupsk.pl/wp-content/uploads/2022/03/${imagePath}`;
   return (
