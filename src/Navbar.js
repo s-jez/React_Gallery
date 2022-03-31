aab3778a636b7b81878928ed48f943ef689f9d1e
stanislawjez <stanislaw.jez@enterosoft.com>
2022-03-31 19:58:18 +0200

    [main] - fix responsive, add media queries

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,32 +1,33 @@
-import './Navbar.css'
-import React, {useState, useEffect} from 'react'
+import "./Navbar.css";
+import React, { useState, useEffect } from "react";
 
 export default function Navbar() {
-    const [toggleMenu, setToggleMenu] = useState(false)
-    const [screenWidth, setScreenWidth] = useState(window.innerWidth)
-    const toggleNav = () => {
-        setToggleMenu(!toggleMenu);
-    }
-    useEffect(() => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [screenWidth, setScreenWidth] = useState(window.innerWidth);
+  const toggleNav = () => {
+    !toggleMenu ? setToggleMenu(true) : setToggleMenu(false);
+  };
+  useEffect(() => {
+    const changeWidth = () => {
+      setScreenWidth(window.innerWidth);
+    };
 
-        const changeWidth = () => {
-          setScreenWidth(window.innerWidth);
-        }
-    
-        window.addEventListener('resize', changeWidth)
-    
-        return () => {
-            window.removeEventListener('resize', changeWidth)
-        }
-      }, [])
-    return (
-        <nav>
-        {(toggleMenu || screenWidth > 500) && (
-                    <ul className="list">
-                        <li className="items">Koło turystyczne - Joanna Adamczak</li>
-                  </ul>
-        )}
-         <button className="btn" onClick={toggleNav}>Menu</button>
-        </nav>
-    )
+    window.addEventListener("resize", changeWidth);
+
+    return () => {
+      window.removeEventListener("resize", changeWidth);
+    };
+  }, []);
+  return (
+    <nav>
+      {(toggleMenu || screenWidth > 500) && (
+        <ul className="list">
+          <li className="items">Koło turystyczne - Joanna Adamczak</li>
+        </ul>
+      )}
+      <button className="btn" onClick={toggleNav}>
+        Menu
+      </button>
+    </nav>
+  );
 }
